【bug fix】Fix large-tensor overflow for exp operator, and mm contiguity issues (#1190)
* [bug fix] fix bug in large shape exp calcullation & mm nan problem

* update attention

* update mm implementation

* update mm implementation

* add large tensor tests case

* add large tensor tests case

* update large shapes

* delete large shape tests

src/flag_gems/ops/attention.py
@@ -922,8 +922,18 @@ def scaled_dot_product_attention_backward(
 
     # NOTE that dk & dv always have the same number of heads as q
     dq = torch.empty_like(query).contiguous()
-    dk = torch.empty((BATCH, Q_HEAD, KV_CTX, HEAD_DIM_K)).to(key.device).contiguous()
-    dv = torch.empty((BATCH, Q_HEAD, KV_CTX, HEAD_DIM_V)).to(value.device).contiguous()
+    dk = torch.empty(
+        (BATCH, Q_HEAD, KV_CTX, HEAD_DIM_K),
+        device=key.device,
+        dtype=key.dtype,
+        memory_format=torch.contiguous_format,
+    )
+    dv = torch.empty(
+        (BATCH, Q_HEAD, KV_CTX, HEAD_DIM_V),
+        device=value.device,
+        dtype=value.dtype,
+        memory_format=torch.contiguous_format,
+    )
 
     _attn_bwd_preprocess[pre_grid](
         o,

src/flag_gems/runtime/backend/_nvidia/hopper/ops/mm.py
@@ -141,13 +141,15 @@ def mm_kernel_general(
         # do matrix multiplication
         rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
         rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
-        ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M)
-        rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N)
+        ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M).to(tl.int64)
+        rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N).to(tl.int64)
+        rm = rm.to(tl.int64)
+        rn = rn.to(tl.int64)
         prev_multiple = prev_multiple_of(K, BLOCK_K)
 
         acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
         for start_k in range(0, prev_multiple, BLOCK_K):
-            rk = start_k + tl.arange(0, BLOCK_K)
+            rk = (start_k + tl.arange(0, BLOCK_K)).to(tl.int64)
             a = tl.load(A + (ram[:, None] * stride_am + rk[None, :] * stride_ak))
             b = tl.load(B + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn))
             if a.dtype != b.dtype:
@@ -156,7 +158,7 @@ def mm_kernel_general(
             acc += tl.dot(a, b, out_dtype=tl.float32, allow_tf32=False)
 
         # loop peeling
-        rk = prev_multiple + tl.arange(0, BLOCK_K)
+        rk = (prev_multiple + tl.arange(0, BLOCK_K)).to(tl.int64)
         mask_k = rk < K
         a = tl.load(
             A + (ram[:, None] * stride_am + rk[None, :] * stride_ak),
@@ -175,8 +177,8 @@ def mm_kernel_general(
 
         acc = acc.to(C.dtype.element_ty)
         # rematerialize rm and rn to save registers
-        rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
-        rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+        rm = (pid_m * BLOCK_M + tl.arange(0, BLOCK_M)).to(tl.int64)
+        rn = (pid_n * BLOCK_N + tl.arange(0, BLOCK_N)).to(tl.int64)
         offsets = C + (rm[:, None] * stride_cm + rn[None, :] * stride_cn)
         mask = (rm < M)[:, None] & (rn < N)[None, :]
         # handles write-back with reduction-splitting
@@ -202,7 +204,7 @@ def get_higher_dtype(a, b):
 
 def general_mm(a, b, c, M, N, K):
     logger.debug(
-        "GEMS MM, [mm scenario]: general, [shape info]: [-, %s, %s, %s](batch, M, N, K), "
+        "GEMS MM-hopper, [mm scenario]: general, [shape info]: [-, %s, %s, %s](batch, M, N, K), "
         "[A column-major]: %s, [B column-major]: %s",
         M,
         N,
@@ -247,6 +249,8 @@ def streamk_scenario(a, b, M, N, K):
         capability[0] == 8
         and a.dtype in [torch.float16, torch.bfloat16]
         and b.dtype in [torch.float16, torch.bfloat16]
+        and a.is_contiguous()
+        and b.is_contiguous()
         and K > M * 5
         and K > N * 5
     )

src/flag_gems/utils/pointwise_dynamic.py
@@ -1134,7 +1134,13 @@ class PointwiseDynamicFunction:
             outputs_dtypes_for_allocation.append(dtype)
 
         tensors = out_tensors + in_tensors
-        if self.use_fast_path(tensors):  # dimension collapse & use physical ordering
+        INT32_MAX = torch.iinfo(torch.int32).max
+        use_fast_path = self.use_fast_path(tensors)
+        fast_path_allowed = (
+            use_fast_path and tensors and tensors[0].numel() <= INT32_MAX
+        )
+        if fast_path_allowed:  # dimension collapse & use physical ordering
+            # if self.use_fast_path(tensors):  # dimension collapse & use physical ordering
             allocated_outputs = [
                 torch.empty_like(tensors[0], dtype=dtype)
                 for dtype in outputs_dtypes_for_allocation